8276c037c3b38c7c05b2cb7941420f065c49d72e
chua0039 <Last93sc2>
2021-09-13 00:36:12 +0800

    Add project files.

---
+++ Assignment5032/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment5032.MvcApplication" Language="C#" %>
---
+++ Assignment5032/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment5032.Startup))]
+namespace Assignment5032
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
